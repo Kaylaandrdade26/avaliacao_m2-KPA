172a58ce3b12160ac7ad3bca7f5f8f711a1291d5
Kaylaandrdade26 <andradekah26@gmail.com>
2025-09-17 19:08:26 -0300

    Update avaliacao.js

--- avaliacao.js
+++ avaliacao.js
@@ -7,7 +7,41 @@ const catalogo = [
   { id: 5, titulo: "Pulp Fiction", ano: 1994, nota: 8.9, genero: "Crime" },
 ];
 
- 
+
+
+// exercicio 01
+/*
+const nomeCatalogo = "CineTech";
+
+
+totalFilmes = filmes.length;
+
+
+console.log ("o total de filmes é: " + totalFilmes);
+
+
+
+
+// exercicio 02
+
+
+ function classificarNota (filmes) {
+        let = "excelente";
+    if (notaFilme >=9 ){
+        return "excelente";
+    }
+    else if (notaFilme >=8 || 8.9){
+        return "Muito Bom";
+ };
+ };
+
+
+console.log(filmes);
+*\
+
+
+// Exercício 03
+
 const listarFilmesDeGenero = (genero) => {
   return catalogo.filter(filme => filme.genero === genero);
 };
@@ -25,3 +59,70 @@ const gerarRelatorioDeFilmes = (anoMinimo) => {
 console.log(listarFilmesDeGenero("Ficção Científica"));
 console.log(gerarListaDeTitulos(catalogo));
 console.log(gerarRelatorioDeFilmes(2000));
+*/
+// Exercício 04
+
+/*const filmesClassicos = filmes.filter(filme => filme.ano < 2000);
+console.log("Filmes Clássicos:", filmesClassicos)
+*/
+
+// Exercício 05
+
+/*const titulosFilmes = filmes.map(filme => filme.titulo);
+console.log("Títulos dos Filmes:", titulosFilmes);
+*/
+
+// Exercício 6
+/*const ficcaoCientificaExcelentes = filmes.filter(
+  filme => filme.genero === "Ficção Científica" && filme.nota >= 8.5
+);
+console.log("Ficção Científica Excelentes:", ficcaoCientificaExcelentes);
+*/
+
+// Exercício 07
+/*const titulosDrama = filmes
+  .filter(filme => filme.genero === "Drama")
+  .map(filme => filme.titulo);
+
+console.log("Títulos de Drama:", titulosDrama);
+*/
+
+// Exercício 08
+/*function existeFilmeDoGenero(genero) {
+  return filmes.some(filme => filme.genero === genero);
+}
+
+console.log("Existe filme de Ação?", existeFilmeDoGenero("Ação"));
+console.log("Existe filme de Drama?", existeFilmeDoGenero("Drama"));
+*/
+
+
+// Exercício 09
+
+/*function buscarEnderecoPorCEP(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.json())
+    .then(data => console.log("Endereço encontrado:", data))
+    .catch(error => console.error("Erro ao buscar o CEP:", error));
+}
+*/
+
+
+//
+// Exercício 10
+/*function encontrarFilmePorId(id) {
+  return filmes.find(filme => filme.id === id);
+}
+
+console.log("Filme com ID 3:", encontrarFilmePorId(3));
+*/
+/*function contarFilmesPorGenero(filmes) {
+  return filmes.reduce((acumulador, filme) => {
+    // se o gênero já existe, soma +1, senão inicia em 1
+    acumulador[filme.genero] = (acumulador[filme.genero] || 0) + 1;
+    return acumulador;
+  }, {});
+}
+
+const relatorioGeneros = contarFilmesPorGenero(filmes);
+console.log("Relatório de Gêneros:", relatorioGeneros);*/
